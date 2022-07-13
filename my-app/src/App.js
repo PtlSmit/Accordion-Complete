a22216e2033ac0f86e63b44c31c9147fe37e3767
Smit <ptlsmit.b@gmail.com>
2022-07-13 15:04:24 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import data from './data'
+import SingleQuestion from './Question'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
